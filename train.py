b9e7bfc2a2ab5cdbf15bff6435396b32d05a4978
Semyon-Yakovlev <patt1337gg@gmail.com>
2023-12-23 08:01:53 +0300

    last one

--- train.py
+++ train.py
@@ -1,10 +1,16 @@
+import subprocess
+
 import hydra
 import joblib
+import mlflow
 import numpy as np
 import pandas as pd
+import toml
 import torch
 from dvc.api import DVCFileSystem
 from omegaconf import DictConfig
+from sklearn.metrics import mean_absolute_percentage_error
+from torcheval.metrics import R2Score
 
 
 class DiamondsDataset(torch.utils.data.Dataset):
@@ -22,54 +28,68 @@ class DiamondsDataset(torch.utils.data.Dataset):
         return self.X[idx], self.y[idx]
 
 
-def train_loop(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    losses = []
-    for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
-        pred = model(X).view(-1)
-        loss = loss_fn(pred, y)
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.detach().numpy())
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-    return np.mean(losses)
-
-
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def train_model(cfg: DictConfig):
-    fs = DVCFileSystem("https://github.com/Semyon-Yakovlev/MLOPS/")
-    with fs.open("data/diamonds.csv") as f:
-        diamonds = DiamondsDataset(f)
-    train_size = int(0.8 * len(diamonds))
-    test_size = len(diamonds) - train_size
-
-    train, test = torch.utils.data.random_split(diamonds, [train_size, test_size])
-    data_train = torch.utils.data.DataLoader(train, batch_size=cfg["params"].batch_size)
-    joblib.dump(diamonds.X[test.indices], "data/X_test.h5")
-
-    X_features = diamonds.X.size()[1]
-    model = torch.nn.Sequential(
-        torch.nn.Linear(X_features, 32),
-        torch.nn.ReLU(),
-        torch.nn.Linear(32, 16),
-        torch.nn.ReLU(),
-        torch.nn.Linear(16, 8),
-        torch.nn.ReLU(),
-        torch.nn.Linear(8, 1),
+    config_path = "mlflow_config.toml"
+    config = toml.load(config_path)
+    mlflow.set_tracking_uri(
+        f"http://{config['server']['host']}:{config['server']['port']}"
     )
+    git_commit_id = (
+        subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+        .strip()
+        .decode("utf-8")
+    )
+    with mlflow.start_run():
+        mlflow.log_param("git_commit_id", git_commit_id)
+        mlflow.log_param("batch_size", cfg["params"].batch_size)
+        mlflow.log_param("learning_rate", cfg["params"].learning_rate)
+        mlflow.log_param("epochs", cfg["params"].epochs)
+        fs = DVCFileSystem("https://github.com/Semyon-Yakovlev/MLOPS/")
+        with fs.open("data/diamonds.csv") as f:
+            diamonds = DiamondsDataset(f)
+        train_size = int(0.8 * len(diamonds))
+        test_size = len(diamonds) - train_size
 
-    loss = torch.nn.MSELoss()
-    losses = []
-    optimizer = torch.optim.Adam(model.parameters(), lr=cfg["params"].learning_rate)
-    for i in range(cfg["params"].epochs):
-        losses.append(train_loop(data_train, model, loss, optimizer))
-    joblib.dump(model, "data/model.h5")
+        train, test = torch.utils.data.random_split(diamonds, [train_size, test_size])
+        data_train = torch.utils.data.DataLoader(
+            train, batch_size=cfg["params"].batch_size
+        )
+        joblib.dump(diamonds.X[test.indices], "data/X_test.h5")
+
+        X_features = diamonds.X.size()[1]
+        model = torch.nn.Sequential(
+            torch.nn.Linear(X_features, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 16),
+            torch.nn.ReLU(),
+            torch.nn.Linear(16, 8),
+            torch.nn.ReLU(),
+            torch.nn.Linear(8, 1),
+        )
+
+        loss_fn = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=cfg["params"].learning_rate)
+        for i in range(cfg["params"].epochs):
+            losses = []
+            mape = []
+            r2score = []
+            for X, y in data_train:
+                # Compute prediction and loss
+                pred = model(X).view(-1)
+                loss = loss_fn(pred, y)
+
+                # Backpropagation
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                losses.append(loss.detach().numpy())
+                mape.append(mean_absolute_percentage_error(pred.detach().numpy(), y))
+                r2score.append(R2Score().update(pred, y).compute())
+            mlflow.log_metric("MAPE_train", np.mean(mape), step=i)
+            mlflow.log_metric("R2Score_train", np.mean(r2score), step=i)
+            mlflow.log_metric("MSELoss_train", np.mean(losses), step=i)
+        joblib.dump(model, "data/model.h5")
 
 
 if __name__ == "__main__":
